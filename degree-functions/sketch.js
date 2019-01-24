fd4c6f3c8cc5787dfaa74587b0478bc2d2f51a14
shutterstacks <mykyta.bychkov@gmail.com>
2019-01-24 13:25:35 -0500

    function thing is basically done now

--- degree-functions/sketch.js
+++ degree-functions/sketch.js
@@ -1,18 +1,31 @@
-let a = 1;
-let wi = 0.5;
+
+let a = 2;
+let wi = 1;
+let b = 200;
+let h = b;
+let k = b;
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(b*2, b*2);
 }
 function draw() {
-  if(keyIsDown(65)) a+=0.01;
-  if(keyIsDown(68)) a-=0.01;
+  a = document.querySelector("#aa").value;
   clear();
+  stroke(100);
+  line(width/2, 0, width/2, width);
+  line(0, width/2, width, width/2, );
   fill(0, 10);
   rect(0, 0, width, height);
-  noStroke();
-  fill(0);
-  for(var x = 0; x <= width; x+=wi) {
-    let y = 400 - x ** a;
-     rect(x, y, wi, wi);
+  stroke(0);
+  noFill();
+  beginShape();
+  for(var x = -h; x <= width; x+=wi) {
+    xx = x/(width/4);
+    let y = 4 - xx ** a;
+     vertex(xx*(width/4) + h, y*(width/4) - k);
    }
+   endShape();
   }
+function keyPressed() {
+  if (keyCode === 65) a = (a*10 + 1)/10;
+  else if (keyCode === 68) a = (a*10 - 1)/10;
+}
