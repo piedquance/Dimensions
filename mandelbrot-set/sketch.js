c0ff52e38f79185c34fd8a721c2f41a8f4d48861
shutterstacks <shutterstacks@gmail.com>
2019-10-19 19:20:22 -0400

    removed math.js library

--- mandelbrot-set/sketch.js
+++ mandelbrot-set/sketch.js
@@ -1,9 +1,11 @@
-let side = 400;
+let side = 800;
 let scale = 2;
 let scaling = scale / (side/2);
 let rad = 5; 
 let color;
 let iterations = 20;
+let k = 0;
+let h = 1;
 
 function setup() {
 createCanvas(side, side);
@@ -12,28 +14,25 @@ createCanvas(side, side);
     rect(0, 0, width, height);
     for(let y = 0; y < side; y++){
         for(let x = 0; x < side; x++){
-            let c = math.complex((x - side/2) * scaling, (-(y - side/2)) * scaling);
+            let c = [(x - side/2) * scaling - h, (-(y - side/2)) * scaling - k];
             let z = c;
-          // console.log(x, y);
-          //  console.log("coordinates : " + z.re, z.im);
+
             for(let int = 0; int < iterations; int++) {
-                if(z.re == Infinity) {
+                if(z[0] == Infinity) {
                     break;
                 }
-                c = math.complex((x - side/2) * scaling, (-(y - side/2)) * scaling);
+                c = [(x - side/2) * scaling - h, (-(y - side/2)) * scaling - k];
 
+                z[0] = z[0]**2 - z[1]**2 + c[0];
+                z[1] = 2 * z[0] * z[1] + c[1];
 
-             // console.log("iteration " + int + " : ", z)
-                z = math.evaluate(`(${z.re} + ${z.im}i)^2 + ${c.re} + ${c.im}i`);
             }
-            if(Math.sqrt(z.re**2 + z.im**2) < rad) {
+            if(Math.sqrt(z[0]**2 + z[1]**2) < rad) {
       
                 color = "#fff";
             } else {
                 color = "#000";
-                
             }
-           // console.log("final : " + z + "\nradius : " + Math.sqrt(z.re**2 + z.im**2) + "\n\n")
             stroke(color);
             rect(x, y, 1, 1);
         }
