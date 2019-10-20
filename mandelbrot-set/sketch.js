3eb87e29ac3168a865e3857e677f6ddd445238bc
shutterstacks <shutterstacks@gmail.com>
2019-10-20 10:11:38 -0400

    removed some code

--- mandelbrot-set/sketch.js
+++ mandelbrot-set/sketch.js
@@ -1,18 +1,16 @@
 let side = 800;
 let scale = 1.5;
-let scaling = scale / (side/2);
+let scaling = scale * 2 /side;
 let rad = 2; 
-const smallItr = (itr, arr) => itr % (arr.length)
 let iterations = 100;
 let k = 0;
 let h = -0.5;
-let itr = new Array(side, side);
+let itr = [];
 function setup() {
 createCanvas(side, side);
     fill('#000');
     stroke("#000");
     rect(0, 0, width, height);
-
     for(let x = -side/2; x < side/2; x++){
         itr[x + side/2] = [];
         for(let y = side/2; y > -side/2; y--){  
@@ -24,20 +22,9 @@ createCanvas(side, side);
                 z[int+3] = ( 2 * z[int] * z[int + 1] ) + c[1]; 
                 if(Math.sqrt(z[int + 2]**2 + z[int + 3]**2) > rad) {
                     itr[x + side/2][y + side/2] = int; 
-                 //   console.log(int)
-                    int = iterations + 1;
-                       } }
-            if(Math.sqrt(z[iterations - 2]**2 + z[iterations - 1]**2) < rad) {
-                color = 0} 
-            else {
-                color = (itr[x + side/2][y + side/2] * 500 / iterations); 
-        //    console.log(itr[x + side/2][y + side/2]) 
-    }
+                    int = iterations + 1; } }
+            (Math.sqrt(z[iterations - 2]**2 + z[iterations - 1]**2) < rad) ? color = 0 : color = (itr[x + side/2][y + side/2] * 500 / iterations); 
             stroke(color);
             rect(x + side/2, y + side/2, 1, 1); } } }
 
-
-            window.addEventListener("load", function(){
-                document.getElementById('canvas').appendChild(document.getElementById('defaultCanvas0'))
-            });
-
+            window.addEventListener("load", () => { document.getElementById('canvas').appendChild(document.getElementById('defaultCanvas0' ) ) });
