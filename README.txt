52e8647a7359e481585e9cb0997585434e2b8058
shutterstacks <shutterstacks@gmail.com>
2020-08-09 09:11:46 -0400

    made the box better

--- README.txt
+++ README.txt
@@ -1,4 +1,5 @@
-Different experiments with dimensions
+A repository for various experiments with p5.js and three.js
+
 Current stuff:
 - A power function viewer
 - A "star" generator
--- script.js
+++ script.js
@@ -1,22 +1,14 @@
-var y = 0.001;
-var x = 0.001;
-
-
 function setup() {
   var myCanvas = createCanvas(windowWidth, windowHeight, WEBGL);  
- // myCanvas.parent('body');
- // background(0);
 }
-
 
 function draw() {
     myCanvas = createCanvas(windowWidth, windowHeight, WEBGL); 
     background(0);
-    rotateX(x);
-    rotateY(y);
-    fill(0,255,100,100);
-		stroke(5);
+    rotateX(frameCount * 0.01);
+    rotateY(frameCount * 0.01);;
+    fill(173,220,202,100);
+		stroke(173, 220, 202);
     box(windowWidth * 0.3);
-    x += random(0.001, 0.005);
-    y += random(0.001, 0.005);
+
   }
